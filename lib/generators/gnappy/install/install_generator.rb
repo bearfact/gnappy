b77671b27da03d6a5035d7ae1a5f48665c27683e
Joey Crone <joey@tragon.com>
2014-12-26 10:38:27 -0600

    proto

--- lib/generators/gnappy/install/install_generator.rb
+++ lib/generators/gnappy/install/install_generator.rb
@@ -5,31 +5,17 @@ module Gnappy
     class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path("../templates", __FILE__)
 
-      desc "Create assets json and gulpfile"
+      desc "Create package.json and gulpfile.js for gnappy projects"
 
       def create_gulp_and_assets_file
-        template 'assets.json', 'assets.json'
         template 'gulpfile.js', 'gulpfile.js'
         template 'package.json', 'package.json'
       end
 
-      def inject_static_server_port
-        insert_into_file 'config/application.rb',
-                         after: "class Application < Rails::Application\n" do
-          out = ''
-          out << '    # static server port number used by gnappy'
-          out << "\n"
-          out << '    # if you change this also change the port in the gulpfile'
-          out << "\n"
-          out << '    config.static_server_port = 5001'
-          out << "\n"
-          out << "\n"
-        end
-      end
 
       def inject_tag_helpers
         insert_into_file 'app/controllers/application_controller.rb',
-                         before: 'end' do
+                         after: "class ApplicationController < ActionController::Base\n" do
           out = ''
           out << '  # tag helpers for gnappy'
           out << "\n"
@@ -42,11 +28,12 @@ module Gnappy
       def inject_git_ignore
         append_to_file ".gitignore" do
           out = ""
+          out << "\n"
           out << "#  ignore gnappy stuff"
           out << "\n"
           out << "/node_modules"
           out << "\n"
-          out << "/bower_components"
+          out << "/vendor/assets/components"
           out << "\n"
           out << "/public/assets/build"
           out << "\n"
--- lib/gnappy/tag_helpers.rb
+++ lib/gnappy/tag_helpers.rb
@@ -2,62 +2,49 @@ require 'json'
 
 module Gnappy
   module TagHelpers
-    def gnappy_javascript_include_tag(file_name)
-      output = ""
+    def gnappy_javascript_include_tag(sources)
+      Rails.logger.info "***************************"
+      Rails.logger.info ENV['GNAPPY_ASSETS_ENV']
       if Rails.env.development? || Rails.env.test?
-        gulp_assets["#{file_name}.js"].each do |path|
-          if path.include? "*"
-            Dir.glob(path).each do|f|
-              output = output + "<script type='text/javascript' src='http://localhost:#{Rails.application.config.static_server_port}/#{f}'></script>"
-            end
-          else
-            output = output + "<script type='text/javascript' src='http://localhost:#{Rails.application.config.static_server_port}/#{path}'></script>"
-          end
-        end
+        javascript_include_tag sources
       else
-        path = '/assets/build/' + gulp_manifest["#{file_name}.js"]
-        output = "<script type='text/javascript' src='#{path}'></script>"
+        output = ''
+        sources = *sources
+        sources.each do |file_name|
+          path = '/assets/build/' + gulp_manifest["#{file_name}.js"]
+          output = output + "<script type='text/javascript' src='#{path}'></script>"
+        end
+        output.html_safe
       end
-      output.html_safe
     end
 
-    def gnappy_stylesheet_include_tag(file_name)
-      output = ""
+    def gnappy_stylesheet_link_tag(sources)
       if Rails.env.development? || Rails.env.test?
-        gulp_assets["#{file_name}.css"].each do |path|
-          if path.include? "*"
-            Dir.glob(path).each do|f|
-              output = output + "<link href='http://localhost:#{Rails.application.config.static_server_port}/#{f}' media='screen' rel='stylesheet'>"
-            end
-          else
-            output = output + "<link href='http://localhost:#{Rails.application.config.static_server_port}/#{path}' media='screen' rel='stylesheet'>"
-          end
-        end
+        stylesheet_link_tag sources
       else
-        path = '/assets/build/' + gulp_manifest["#{file_name}.js"]
-        output = "<link href='#{path}' media='screen' rel='stylesheet'>"
+        output = ''
+        sources = *sources
+        sources.each do |file_name|
+          path = '/assets/build/' + gulp_manifest["#{file_name}.css"]
+          output = output + "<link href='#{path}' media='screen' rel='stylesheet'>"
+        end
+        output.html_safe
       end
-      output.html_safe
     end
+
+
 
     private
 
-    def gulp_assets
-      @assets ||= if File.exist?(Rails.root.join('assets.json'))
-                    file = File.read(Rails.root.join('assets.json'))
-                    @assets = JSON.parse(file)
-                  else
-                    {}
-                  end
-    end
-
     def gulp_manifest
-      @manifest ||= if File.exist?(Rails.root.join('asset_manifest.json'))
-                      file = File.read(Rails.root.join('asset_manifest.json'))
+      @manifest ||= if File.exist?(Rails.root.join('rev-manifest.json'))
+                      file = File.read(Rails.root.join('rev-manifest.json'))
                       @manifest = JSON.parse(file)
                     else
                       {}
                     end
+      Rails.logger.info @manifest
+      @manifest
     end
   end
 end
